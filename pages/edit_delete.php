c70a01c2d52265bd7fac66ddb5b735f128810a6d
Daniil Motsnyi <daniilmotsnyi@gmail.com>
2020-11-24 09:18:45 +0200

    photo

--- pages/edit_delete.php
+++ pages/edit_delete.php
@@ -1,22 +1,25 @@
 <?php
     require_once "db/connection.php"; // conn
+    $id = mysqli_real_escape_string($conn, $_REQUEST['id']);
     $first_name = mysqli_real_escape_string($conn, $_REQUEST['first_name']);
     $last_name = mysqli_real_escape_string($conn, $_REQUEST['last_name']);
     $password = mysqli_real_escape_string($conn, $_REQUEST['pass']);
+    $role_id = mysqli_real_escape_string($conn, $_REQUEST['role_id']);
+    $email = mysqli_real_escape_string($conn, $_REQUEST['email']);
     session_start();
 
     if (count($_POST)>0) {
-      if($_POST['user_edit']){
+      if($_POST['edit']){
         $sql = "UPDATE users SET first_name='$first_name', last_name='$last_name',
-        password='$password' WHERE id=".$_GET['id'];
+        password='$password', role_id='$role_id', email='$email' WHERE id='$id'";
         $res = mysqli_query($conn, $sql);
-        header('Location: pages/restricted.php?id='.$_SESSION['id']);
+        header('Location: pages/restricted.php?id='.$_SESSION['admin_id']);
       }
 
-      if($_POST['user_delete']){
-        $sql = "DELETE FROM users WHERE id=".$_GET['id'];
+      if($_POST['delete']){
+        $sql = "DELETE FROM users WHERE id='$id'";
         $res = mysqli_query($conn, $sql);
-        header('Location: ../index.php');
+        header('Location: pages/restricted_admin.php?id='.$_SESSION['admin_id']);
       }
 
       if (!$res) {
--- pages/user.php
+++ pages/user.php
@@ -4,8 +4,6 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Account</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <body>
   <header>
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
@@ -21,7 +19,7 @@
                 $url = "/";
               }
             ?>
-            <a class="btn btn-outline-info" href="<?php echo $url; ?>">Back</a>
+            <a class="btn btn-outline-primary" href="<?php echo $url; ?>">Back</a>
         </nav>
     </div>
   </header>
@@ -29,17 +27,23 @@
     <section class="overlay">
         <?php
           require_once "../db/connection.php";
-          $sql_user = "SELECT id, first_name, last_name, password FROM users WHERE id=".$_GET['id'];
+          $sql_user = "SELECT id, first_name, last_name, password, photo FROM users WHERE id=".$_GET['id'];
           $result_user = $conn->query($sql_user);
 
           if ($result_user->num_rows > 0) {
               while($row_user = $result_user->fetch_assoc()) { ?>
-                <h3>Account info</h3>
-                <form action="../user_edit_delete.php" method="post">
-                  <label for="id">Id:</label>
-                  <br>
-                  <input id="id" class="form-control" name="id" type="text" value="<?php echo $row_user['id']; ?>" minlength="1" maxlength="20">
-                  <br>
+                <h3>Account info</h3><br>
+                <img src="../public/images/<?php echo $row_user['photo']; ?>" class="rounded" alt="This is place for your photo">
+                <form action="../upload.php?id=<?php echo $_GET['id']; ?>" method="post" enctype="multipart/form-data">
+                  <br> Select image to upload:<br><br>
+                  <div class="custom-file">
+                    <input id="fileToUpload" name="fileToUpload" type="file" class="custom-file-input" data-show-preview="false">
+                    <label class="custom-file-label" for="customFile">Choose file</label>
+                  </div>
+                  <br><br>
+                  <input class="btn btn-sm btn-primary" type="submit" value="Upload Image" name="submit"><br>
+                </form>
+                <form action="../user_edit_delete.php?id=<?php echo $_GET['id']; ?>" method="post">
                   <label for="first_name">First name:</label>
                   <br>
                   <input id="first_name" class="form-control" name="first_name" type="text" value="<?php echo $row_user['first_name']; ?>" minlength="2" maxlength="20" pattern="[a-zA-Z]+">
@@ -51,10 +55,6 @@
                   <label for="pass">Password:</label>
                   <br>
                   <input id="pass" class="form-control" name="pass" type="text" value="<?php echo $row_user["password"]; ?>" minlength="6" maxlength="32">
-                  <br>
-                  <label for="pass_rep">Repeat password:</label>
-                  <br>
-                  <input id="pass_rep" class="form-control" name="pass" type="text" minlength="6" maxlength="32">
                   <br><br>
                   <nav class="my-2 my-md-0 mr-md-3">
                       <input id="submit" class="btn btn-primary" name="user_edit" type="submit" value="Edit">
@@ -68,3 +68,6 @@
     </section>
   </div>
 </body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
